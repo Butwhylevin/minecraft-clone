9812116977e6758f8459dd2a52b4baed4facef08
Butwhylevin <levinpaulward@gmail.com>
2020-10-22 20:13:42 -0500

    idk

--- Assets/Scripts/WorldGen.cs
+++ Assets/Scripts/WorldGen.cs
@@ -22,9 +22,34 @@ public class WorldGen : MonoBehaviour
         GenerateTerrain();
     }
 
-    void Update()
+    void Combine(GameObject block)
     {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+        //Destroy(this.gameObject.GetComponent<MeshCollider>());
+
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+            i++;
+        }
         
+        MeshFilter filter = transform.GetComponent<MeshFilter>();
+        filter.mesh = new Mesh();
+        
+        filter.mesh.CombineMeshes(combine,true);
+        filter.mesh.RecalculateBounds();
+        filter.mesh.RecalculateNormals();
+        filter.mesh.Optimize();
+
+        //this.gameObject.AddComponent<MeshCollider>();
+        this.gameObject.GetComponent<MeshCollider>().sharedMesh = filter.mesh;
+        transform.gameObject.SetActive(true);
+
+        Destroy(block);
     }
 
     void GenerateTerrain()
@@ -37,11 +62,24 @@ public class WorldGen : MonoBehaviour
                 maxY += groundHeight;
                 GameObject grass = Instantiate(blocks[0], new Vector3(x,maxY,z), Quaternion.identity) as GameObject;
                 grass.transform.SetParent(GameObject.FindGameObjectWithTag("Enviroment").transform);
+                Combine(grass);
 
-                for (int y = 0; y < maxY; y++)
+                for (int y = 0; y < maxY; y++) //dirt
                 {
-                    GameObject stone = Instantiate(blocks[1], new Vector3(x,y,z), Quaternion.identity) as GameObject;
-                    stone.transform.SetParent(GameObject.FindGameObjectWithTag("Enviroment").transform);
+                    int dirtLayers = Random.Range(3,5);
+                    if(y >= maxY - dirtLayers)
+                    {
+                        GameObject dirt = Instantiate(blocks[1], new Vector3(x,y,z), Quaternion.identity) as GameObject;
+                        dirt.transform.SetParent(GameObject.FindGameObjectWithTag("Enviroment").transform);
+                        //Combine(dirt);
+                    }
+                    else //stone
+                    {
+                        GameObject stone = Instantiate(blocks[2], new Vector3(x,y,z), Quaternion.identity) as GameObject;
+                        stone.transform.SetParent(GameObject.FindGameObjectWithTag("Enviroment").transform);
+                        //Combine(stone);
+                    }
+                   
                 }
 
                 if(x ==(int)(sizeX / 2) && z == (int)(sizeZ / 2))
@@ -50,5 +88,14 @@ public class WorldGen : MonoBehaviour
                 }
             }
         }
+        //Simplify(0.5f);
+    }
+    public void Simplify(float quality)
+    {
+        MeshFilter filter = transform.GetComponent<MeshFilter>();
+        var meshSimplifier = new UnityMeshSimplifier.MeshSimplifier();
+        meshSimplifier.Initialize(filter.mesh);
+        meshSimplifier.SimplifyMesh(quality);
+        filter.mesh = meshSimplifier.ToMesh();
     }
 }
