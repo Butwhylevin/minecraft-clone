0f4de2141a7535432d571e1f27916d1c80aeec86
Butwhylevin <levinpaulward@gmail.com>
2020-10-27 22:01:00 -0500

    Started from scratch with new tutorial

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+    int vertexIndex = 0;
+    List<int> triangles = new List<int>();
+    List<Vector3> vertices = new List<Vector3>();
+    List<Vector2> uvs = new List<Vector2>();
+
+    void Start()
+    {
+        AddVoxelDataToChunk();
+        CreateMesh();
+    }
+
+    void AddVoxelDataToChunk(Vector3 pos)
+    {
+        for (int p = 0; p<6; p++)
+        {
+            for (int i = 0; i<6; i++)
+            {
+                int triangleIndex = VoxelData.voxelTris[p,i];
+                vertices.Add(VoxelData.voxelVerts[triangleIndex] + pos);
+                triangles.Add(vertexIndex);
+
+                uvs.Add(VoxelData.voxelUvs[i]);
+
+                vertexIndex++;
+            }
+        }
+    }
+
+    void CreateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
+
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+    }
+}
